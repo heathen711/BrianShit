6e5468a416fc608fc419b3213598a8a601534e37
Jay Hill <jay.hill@apple.com>
2018-12-29 14:53:50 -0800

    WIP

--- setup_new_mac.py
+++ setup_new_mac.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import argparse
+import random
 import shlex
 import subprocess
 
@@ -7,9 +9,59 @@ def run_and_check(command):
     print "Executing: {}".format(command)
     result = subprocess.check_output(shlex.split(command))
     print "Output:\n{}".format(result)
+    return result.strip()
 
-commands = [
 
-]
-for command in commands:
-    run_and_check(command)
+def sudo_command(command):
+    return run_and_check("SUDO_ASKPASS=/tmp/ask_pass.sh; sudo -A {}".format(command))
+
+
+def make_user(username, real_name, password, admin=False):
+    uid = random.randint(1010, 3000)
+    gid = 1000
+    if admin:
+        gid = 20
+
+    commands = [
+        "dscl . -create /Users/{username}",
+        "dscl . -create /Users/{username} UserShell /bin/bash",
+        "dscl . -create /Users/{username} RealName {real_name}",
+        "dscl . -create /Users/{username} UniqueID {uid}",
+        "dscl . -create /Users/{username} PrimaryGroupID {gid}",
+        "dscl . -create /Users/{username} NFSHomeDirectory /Local/Users/{username}",
+        "dscl . -passwd /Users/{username} {password}",
+    ]
+    for command in commands:
+        sudo_command(
+            command.format(
+                username=username,
+                password=password,
+                gid=gid,
+                uid=uid,
+                real_name=shlex.quote(real_name)
+            )
+        )
+
+    if admin:
+        sudo_command("dscl . -append /Groups/admin GroupMembership {username}".format(username=username))
+
+
+def main():
+    run_and_check("echo 'echo {}' > /tmp/ask_pass.sh; chmod +x /tmp/ask_pass.sh".format("Il0v3Mamab3@r!"))
+
+    try:
+        make_user("Teacher", "Teacher", "T3@ch3r2013", True)
+        if "F1" in run_and_check("hostname"):
+            make_user("tartan", "tartan", "beyonce*")
+
+        commands = [
+            "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart "\
+                "-activate -configure -allowAccessFor -specifiedUsers"
+            "/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart "\
+                "-activate -configure -access -on -privs -ControlObserve -TextMessages -DeleteFiles "\
+                "-OpenQuitApps -GenerateReports -RestartShutDown -SendFiles -ChangeSettings -users admin,teacher"
+        ]
+        for command in commands:
+            sudo_command(command)
+    finally:
+        run_and_check("rm /tmp/ask_pass.sh")
